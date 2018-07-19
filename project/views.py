97c94daade387c998b94a4649e8f85372a508689
dhkim2013 <dhkim20157@gmail.com>
2018-07-19 17:04:40 +0900

    Upgrade zappa & Add MutiPartParser

--- project/views.py
+++ project/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics, status
+from rest_framework.parsers import FileUploadParser, MultiPartParser
 from rest_framework.response import Response
 
 from project.models import Project, Slide
@@ -15,3 +16,12 @@ class ProjectListCreateAPIView(generics.ListCreateAPIView):
 class SlideListCreateAPIView(generics.ListCreateAPIView):
     queryset = Slide.objects.all()
     serializer_class = SlideSerializer
+    parser_classes = (MultiPartParser, )
+
+    def create(self, request, *args, **kwargs):
+        print(request.data['image'])
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
