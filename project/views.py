df2226be53faa3cd82b5b5289fc613500346646e
dhkim2013 <dhkim20157@gmail.com>
2018-07-20 11:49:36 +0900

    Update that now media file will uploaded to s3 bucket

--- project/views.py
+++ project/views.py
@@ -16,12 +16,3 @@ class ProjectListCreateAPIView(generics.ListCreateAPIView):
 class SlideListCreateAPIView(generics.ListCreateAPIView):
     queryset = Slide.objects.all()
     serializer_class = SlideSerializer
-    parser_classes = (MultiPartParser, )
-
-    def create(self, request, *args, **kwargs):
-        print(request.data['image'])
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
